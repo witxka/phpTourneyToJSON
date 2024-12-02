71e290646e2a6c8c94f36ccc30ef06e6e4c9c340
Victor Kotlyar <witxka@gmail.com>
2024-12-02 16:42:27 +0300

    Some fixes for notify to handle exception and minus in frag scores

--- phpTourneyNotify.py
+++ phpTourneyNotify.py
@@ -4,6 +4,8 @@
 import json
 import datetime
 import requests
+import traceback
+import logging
 from time import sleep
 
 SLEEP_TIME = 5*60
@@ -14,28 +16,33 @@ TOURNEY = "RQWL 17"
 
 # global var to track last confirmed
 lastConfirmed = datetime.datetime.now().isoformat() + "+00:00"
-lastConfirmed = datetime.datetime.fromisoformat("2024-11-23T20:26:58+00:00").isoformat()
+#lastConfirmed = datetime.datetime.fromisoformat("2024-12-02T10:24:07+00:00").isoformat()
 
 def send_message(message):
   print("DEBUG: send_message", message)
 
-  TOKEN = '---------------'
-  CHAT_ID = '--------------'
+  TOKEN = '***
+  CHAT_ID = '***'
   SEND_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
   requests.post(SEND_URL, json={'chat_id': CHAT_ID, 'text': message})
 
 def main():
   global lastConfirmed 
   with open('phpTourney.json') as file:
-    print("DEBUG", lastConfirmed)  
-    data = json.load(file)
+    print("DEBUG", lastConfirmed)
+    try:
+      data = json.load(file)
+    except Exception as e:
+      print("WARN: json.load exception")
+      logging.error(traceback.format_exc())
+      return
     for match in data:
       if match["confirmed"] > lastConfirmed:
         sendMessage =  TOURNEY +": " + match["players"][0] + " vs " + match["players"][1] \
           + " [" + match["info"][0].replace(" ", "") + "]"
 
         for i in range(1,len(match["info"]),2):
-          sendMessage += " " + match["info"][i+1].replace(" ", "").replace("-", ":") \
+          sendMessage += " " + match["info"][i+1].replace(" - ", ":") \
             + " @ " + match["info"][i] + " |"
         sendMessage += " (" + match["round"] + ") "     
         sendMessage += match["link"]
